38d2e9b9e2d8820d7842bd2744385fbc287e5bb7
Ankit <ankitrajsrivastava06@gmail.com>
2015-12-17 01:16:22 +0530

    added area of circle program

---
+++ area of circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+     float main()
+ {
+     float pi=3.14,r,area;
+     printf("Enter thr redius of the circle\n");
+     scanf("%f",&r);
+     area=3.14*r*r;
+     printf("\nEnter the area or the circle%f",area);
+     return 0;
+     getch();
+     
+ }
+
+
