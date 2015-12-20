e3a7e7154561d129e8a6326f563a4be6cdd0f657
Ankit <ankitrajsrivastava06@gmail.com>
2015-12-20 14:31:03 +0530

    Area of the Rectangle

---
+++ Area of the Rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+  {
+      int l,b,area;
+      
+      printf("Enter the length and breath of the Rectangle\n\n");
+      scanf("%d%d",&l,&b);
+      area=l*b;
+      
+      printf("Area of the Rectangle\n\n%d",area);
+      return 0;
+  }
