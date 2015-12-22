4dbe01563952407eac392a96da77abe7e73c9c9d
Ankit <ankitrajsrivastava06@gmail.com>
2015-12-22 20:48:35 +0530

    greatest of two

---
+++ Find the greatest number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ int main()
+  {
+      int num1,num2;
+      printf("\nEnter two numbers");
+      scanf("%d%d",&num1,&num2);
+     
+     if(num1>num2)
+     printf("\nnum1 is the greatest number%d",num1);
+     else
+         printf("\nnum2 is the greatest number5d",num2);
+         
+         return 0;
+
+  }
+
+
