aefa55c1c5e5ce0f6853a1e5718a981379a5dbd3
ankit0612 <ankitrajsrivastava06@gmail.com>
2015-12-25 22:31:25 +0530

    changed for c-moment

--- area of circle.c
+++ area of circle.c
@@ -2,7 +2,7 @@
      float main()
  {
      float pi=3.14,r,area;
-     printf("Enter thr redius of the circle\n");
+     printf("Enter the redius of the circle\n");
      scanf("%f",&r);
      area=3.14*r*r;
      printf("\nEnter the area or the circle%f",area);
