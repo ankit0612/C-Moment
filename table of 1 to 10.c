d56ad7a5ae5bd739c996bbba921e8a624bd63408
ankit0612 <ankitrajsrivastava06@gmail.com>
2015-12-30 23:50:04 +0530

    table of 1 to 10

---
+++ table of 1 to 10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ void main()
+   {
+       int i ,j ,table;
+       
+       for(i=1;i<=10;i++)
+       {
+           for(j=1;j<=10;j++)
+           {
+               table = i * j ;
+               
+               printf("\t%d",table);
+               
+           }
+       }
+   }
+
+
+
+
+
+
